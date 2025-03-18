3c24eba08e7ba7afa02a040396407c8f36099fcf
aaron-KT <aarons.intern@kaditinnovations.com>
2025-03-18 13:12:34 +0530

    new change

--- src/main/java/com/postgresql/demo/activities/HospitalActivitiesImpl.java
+++ src/main/java/com/postgresql/demo/activities/HospitalActivitiesImpl.java
@@ -14,6 +14,7 @@ public class HospitalActivitiesImpl implements HospitalActivities {
 
     public HospitalActivitiesImpl(HospitalPersistenceService hospitalPersistenceService) {
         this.hospitalPersistenceService = hospitalPersistenceService;
+        
     }
 
     @Override
